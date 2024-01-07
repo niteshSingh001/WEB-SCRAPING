7100defb50bc6de22df89c65c7efbc5e5127312c
Nitesh Singh <ns368497@gmail.com>
2024-01-07 11:21:13 +0530

    added loading animation in frontend.js

--- client/frontend.js
+++ client/frontend.js
@@ -1,15 +1,24 @@
 async function fetchSummary() {
   const urlInput = document.getElementById("urlInput").value;
+  const summaryContainer = document.getElementById("summaryContainer");
 
-  const response = await fetch("https://summarize-eqj8.onrender.com/scrape", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ url: urlInput }),
-  });
+  summaryContainer.innerHTML = "<h3>Loading...</h3>";
+  try {
+    const response = await fetch("https://summarize-eqj8.onrender.com/scrape", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: urlInput }),
+    });
 
-  const data = await response.json();
-  document.getElementById("summaryContainer").innerText = data.summary;
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    const data = await response.json();
+    summaryContainer.innerText = data.summary;
+  } catch (error) {
+    summaryContainer.innerText = `Error: ${error.message}`;
+  }
   document.getElementById("urlInput").value = "";
 }
